eed01542584260e54ccd311d36104fd4c51e1915
Gabriel Vukovic <gabriel.vukovic@student.tugraz.at>
2021-05-25 17:08:18 +0200

    reddit script

--- scripts/reddit.com.js
+++ scripts/reddit.com.js
@@ -10,11 +10,29 @@ module.exports = {
             .waitForElementPresent('body')
             .waitForElementPresent('partial link text', 'Log In')
             .click('partial link text', 'Log In')
-            .waitForElementPresent('button[type=submit]')
-            .setValue('partial link text', 'Username', userName)
-            .setValue('#loginUsername', userName)
-            .setValue('#loginPassword', oldPasswd)
-            .click('button[type=submit]')
+            .waitForElementPresent('#POPUP_CONTAINER')
+            .pause(5000)
+        //var f = browser.element('css selector', '._25r3t_lrPF3M6zD2YkWvZU');
+            .frame(1)
+            .waitForElementVisible('button.AnimatedForm__submitButton.m-full-width')
+            .click('partial link text', 'User Agreement')
+            .setValue('input[name=username]', userName)
+            .setValue('input[name=password]', oldPasswd)
+            .pause(5000)
+            .click('button.AnimatedForm__submitButton.m-full-width')
+        //var frame = browser.element('iframe._25r3t_lrPF3M6zD2YkWvZU');
+
+            // .getAttribute('iframe._25r3t_lrPF3M6zD2YkWvZU', 'id', (result) => {
+            //     console.log(result.value);
+            //     browser
+            //         .frame(result.value)
+            //         .waitForElementPresent('button[type=submit]')
+            //         .setValue('input[name=username]', userName)
+            //         .setValue('input[name=password]', oldPasswd)
+            //         .click('button[type=submit]')
+            // })
+            .pause(5000)
+            .frame(null)
             .waitForElementPresent('div.header-user-dropdown')
             .click('div.header-user-dropdown')
             .waitForElementPresent('partial link text', 'User Settings')
